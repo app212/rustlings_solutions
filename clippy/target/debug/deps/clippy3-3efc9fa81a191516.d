247d9ff598bbeacf07c37a19cbf6a52c7f24dbd6
apiotrowski <adam.piotrowski212@gmail.com>
2023-04-22 17:34:04 +0200

    finished solutions

---
+++ clippy/target/debug/deps/clippy3-3efc9fa81a191516.d
@@ -0,0 +1,5 @@
+/home/adam/Dev/Rust/rustlings_solutions/clippy/target/debug/deps/clippy3-3efc9fa81a191516.rmeta: clippy3.rs
+
+/home/adam/Dev/Rust/rustlings_solutions/clippy/target/debug/deps/clippy3-3efc9fa81a191516.d: clippy3.rs
+
+clippy3.rs:
---
+++ clippy/target/debug/deps/clippy3-ef4c2e8a8fffe72d.d
@@ -0,0 +1,5 @@
+/home/adam/Dev/Rust/rustlings_solutions/clippy/target/debug/deps/clippy3-ef4c2e8a8fffe72d.rmeta: clippy3.rs
+
+/home/adam/Dev/Rust/rustlings_solutions/clippy/target/debug/deps/clippy3-ef4c2e8a8fffe72d.d: clippy3.rs
+
+clippy3.rs:
